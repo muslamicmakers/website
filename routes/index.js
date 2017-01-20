c2f86a73acefa54c9f60861a71cd0c8b2e46c1aa
mrmurtz <murtaza.s.abidi@gmail.com>
2017-01-20 10:45:03 +0000

    Added partials and template for default page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Muslamic Makers' });
+  res.render('default', { title: 'Muslamic Makers' });
 });
+
+router.get('/sponsor', function(req, res, next) {
+  res.render('default', { title: 'Sponsor' });
+});
+
 
 module.exports = router;

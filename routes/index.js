57c4c822dc261a9444ae8d6f48137fd0da03f5fa
mrmurtz <murtaza.s.abidi@gmail.com>
2017-01-20 12:59:45 +0000

    Basic json file, added team images in public

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('default', { title: 'Muslamic Makers' });
+  res.render('index', {
+    title: 'Muslamic Makers'
+  });
 });
 
 router.get('/sponsor', function(req, res, next) {

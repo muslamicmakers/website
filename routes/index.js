f6087cbf4712a7a1f4339075cf6b2b5a4b110c11
mrmurtz <murtaza.s.abidi@gmail.com>
2017-01-29 12:59:06 +0000

    updated gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var data = require('../data.json');
+var request = require('request');
+var url = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + CHANNEL_ID + "&maxResults=10&order=date&type=video&key=" + YOUR_API_KEY;
+
+app.get('/', function(req, res){
+  request(url, function(err, body){
+    console.log(body);
+      res.json(body);
+  });
+})
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
